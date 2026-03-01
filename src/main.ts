2879a019fe115eb7302cccf6254bd5accf1fd2bf
danielgstephenson <dgstephenson.econ@gmail.com>
2026-03-01 18:03:55 -0500

    antialias

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { Game } from './game/game'
 
 async function start(): Promise<void> {
   const app = new Application()
-  await app.init({ background: 'hsl(0, 0%, 0%)', resizeTo: window })
+  await app.init({
+    background: 'hsl(0, 0%, 0%)',
+    resizeTo: window,
+    antialias: true,
+  })
   const pixiDiv = document.getElementById('pixiDiv')!
   pixiDiv.appendChild(app.canvas)
   new Game(app)
